84418f7f93550eaa5fd46bc610436af35bcc8d06
Jason Beedle <jbeedle@gmail.com>
2020-05-27 05:00:32 +0100

    firebase pods added

--- PutneyCricketClub/AppDelegate.swift
+++ PutneyCricketClub/AppDelegate.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Firebase
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -15,6 +16,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        // Call Firebase before the return
+        FirebaseApp.configure()
+        
         return true
     }
 
